40110b689725a5ab00ca806b92098763a0245483
Administrator <admin@example.com>
2020-06-05 11:06:07 +0300

    +Variables

---
+++ Variables.py
@@ -0,0 +1,21 @@
+# -----------------------------
+# Program by Samodelkin
+# -----------------------------
+
+a = "Hello"
+b = 29
+
+print(a)
+print(b)
+
+f_name = "Aleksey"
+s_name = "Samodelkin"
+age = 29
+
+print(f_name + " " + s_name)
+
+full_name = f_name + " " + s_name
+
+print(full_name + " " + str(age))
+
+
